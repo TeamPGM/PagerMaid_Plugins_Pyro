7833d7ba81744d16dee69503d2e9441177a7bf2c
xtaodada <xtao@xtaolink.cn>
2022-06-08 16:58:40 +0800

    修复 Github Action

---
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,21 @@
+## 该 PR 相关 Issue / Involved issue
+
+Close #
+
+## 插件名称 / Name
+
+```
+
+```
+
+## 更改检查表 / Change Checklist
+  
+- [ ] 有插件版本号更新
+- [ ] 有多语言支持
+  - [ ] CN
+  - [ ] EN
+- [ ] 会触发频率限制 Will trigger a rate limit?
+  - [ ] 如果会, 是否有对应的措施? If yes, do your code reflect this sign?
+- [ ] 添加了新的依赖包 New package added
+
+## 说明 / Note
---
+++ telegram_update.py
@@ -0,0 +1,22 @@
+import json, time, sys
+from httpx import get, post
+
+token = str(sys.argv[1])
+main = json.loads(get("https://api.github.com/repos/TeamPGM/PagerMaid_Plugins_Pyro/commits/v2").content)
+text = "#更新日志 #pyro #" + main['commit']['author']['name'].replace('_', '') + \
+       ' \n\n🔨 [' + main['sha'][0:7] + '](https://github.com/TeamPGM/PagerMaid_Plugins_Pyro/commit/' + \
+       main['sha'] + '): ' + main['commit']['message']
+push_content = {'chat_id': '-1001441461877', 'disable_web_page_preview': 'True', 'parse_mode': 'markdown',
+                'text': text}
+url = 'https://api.telegram.org/bot' + token + '/sendMessage'
+try:
+    main_req = post(url, data=push_content)
+except:
+    pass
+push_content['chat_id'] = '-1001319957857'
+time.sleep(1)
+try:
+    main_req = post(url, data=push_content)
+except:
+    pass
+print(main['sha'] + " ok！")
