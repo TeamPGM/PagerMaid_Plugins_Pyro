1600d84d251a17a463f7a99d98df0da978fc1cdc
omg-xtao <100690902+omg-xtao@users.noreply.github.com>
2022-06-09 20:13:21 +0800

    Update main.py

--- diss/main.py
+++ diss/main.py
@@ -1,19 +1,12 @@
-from asyncio import sleep
-from requests import get
 from pyrogram import Client
 from pagermaid.listener import listener
-from pagermaid.utils import Message
+from pagermaid.utils import Message, client, edit_delete
 
 
 @listener(command="diss", description="儒雅随和版祖安语录。")
-async def diss(_: Client, context: Message):
-    await context.edit("获取中 . . .")
+async def diss(_: Client, message: Message):
     for i in range(5):  # 最多尝试5次
-        req = get("https://api.oddfar.com/yl/q.php?c=1009&encode=text")
+        req = await client.get("https://api.oddfar.com/yl/q.php?c=1009&encode=text")
         if req.status_code == 200:
             return await context.edit(req.text)
-        else:
-            continue
-    await context.edit("出错了呜呜呜 ~ 试了好多好多次都无法访问到 API 服务器 。")
-    await sleep(2)
-    await context.delete()
+    await edit_delete(message, "出错了呜呜呜 ~ 试了好多好多次都无法访问到 API 服务器 。")
