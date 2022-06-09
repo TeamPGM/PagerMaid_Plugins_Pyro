af42425b34fbab28d9fff16b0774a20d937676e6
omg-xtao <100690902+omg-xtao@users.noreply.github.com>
2022-06-09 20:15:15 +0800

    Update main.py

--- diss/main.py
+++ diss/main.py
@@ -8,5 +8,5 @@ async def diss(_: Client, message: Message):
     for i in range(5):  # 最多尝试5次
         req = await client.get("https://api.oddfar.com/yl/q.php?c=1009&encode=text")
         if req.status_code == 200:
-            return await context.edit(req.text)
+            return await message.edit(req.text)
     await edit_delete(message, "出错了呜呜呜 ~ 试了好多好多次都无法访问到 API 服务器 。")
