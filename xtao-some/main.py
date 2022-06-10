6df8b041e4b062927b6ce20dd24f2299abf1d3cb
zkysimon <zkygo@outlook.com>
2022-06-10 22:17:23 +0800

    Update main.py
    新增wiki

--- xtao-some/main.py
+++ xtao-some/main.py
@@ -1,9 +1,19 @@
 import json
-
+from requests import get
 from pyrogram import Client
 
 from pagermaid.listener import listener
 from pagermaid.utils import alias_command, Message, client
+
+
+async def obtain_message(context) -> str:
+    reply = context.reply_to_message
+    message = context.arguments
+    if reply and not message:
+        message = reply.text
+    if not message:
+        raise ValueError(lang('msg_ValueError'))
+    return message
 
 
 @listener(is_plugin=True, outgoing=True, command=alias_command("guess"),
@@ -32,3 +42,33 @@ async def guess(c: Client, message: Message):
         await message.edit("\n\n".join(guess_res))
     else:
         await message.edit("没有匹配到拼音首字母缩写")
+
+
+@listener(is_plugin=True, outgoing=True, command="wiki", description="查询维基百科词条", parameters="<词组>")
+async def wiki(_: Client, context: Message):
+    await context.edit("获取中 . . .")
+    try:
+        message = await obtain_message(context)
+    except ValueError:
+        await context.edit("出错了呜呜呜 ~ 无效的参数。")
+        return
+    try:
+        wiki_json = json.loads(get("https://zh.wikipedia.org/w/api.php?action=query&list=search&format=json&formatversion=2&srsearch=" + message).content.decode(
+            "utf-8"))
+    except:
+        await context.edit("出错了呜呜呜 ~ 无法访问到维基百科。")
+        return
+    try:
+        if not len(wiki_json['query']['search']) == 0:
+            wiki_title = wiki_json['query']['search'][0]['title']
+            wiki_content = wiki_json['query']['search'][0]['snippet'].replace('<span class=\"searchmatch\">',
+                                                                              '**').replace(
+                '</span>', '**')
+            wiki_time = wiki_json['query']['search'][0]['timestamp'].replace('T', ' ').replace('Z', ' ')
+            message = '词条： [' + wiki_title + '](https://zh.wikipedia.org/zh-cn/' + wiki_title + ')\n\n' + \
+                      wiki_content + '...\n\n此词条最后修订于 ' + wiki_time
+        else:
+            message = "没有匹配到相关词条"
+    except KeyError:
+        message = wiki_json['error']['info']
+    await context.edit(message)
