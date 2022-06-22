4238166fcc4fd68297b453e24124d858c5fbca37
omg-xtao <100690902+omg-xtao@users.noreply.github.com>
2022-06-22 12:08:50 +0800

    Update dme.py

--- dme/dme.py
+++ dme/dme.py
@@ -1,13 +1,12 @@
-from pyrogram import Client
 from pagermaid.listener import listener
-from pagermaid.utils import Message, lang, alias_command
+from pagermaid.utils import Message, lang
 from pagermaid.modules.prune import self_prune
 
 
-@listener(is_plugin=False, outgoing=True, command=alias_command("dme"),
+@listener(is_plugin=False, command="dme",
           need_admin=True,
           description=lang('sp_des'),
           parameters=lang('sp_parameters'))
-async def dme(_: Client, message: Message):
+async def dme(message: Message):
     """ Deletes specific amount of messages you sent. """
-    await self_prune(_, message)
+    await self_prune(message)
