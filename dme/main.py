65aa1d865f1c3dcf765f8a202a40b03ec56a2432
Bascter-Main <25437043+Bascter-Main@users.noreply.github.com>
2022-06-22 12:26:54 +0800

    Update main.py

--- dme/main.py
+++ dme/main.py
@@ -1,12 +1,13 @@
+""" Module to automate message deletion. """
 from pagermaid.listener import listener
-from pagermaid.utils import Message, lang
+from pagermaid.utils import Message, lang, alias_command
 from pagermaid.modules.prune import self_prune
 
 
-@listener(is_plugin=False, command="dme",
+@listener(is_plugin=False, outgoing=True, command=alias_command("dme"),
           need_admin=True,
           description=lang('sp_des'),
           parameters=lang('sp_parameters'))
-async def dme(message: Message):
+async def dme(_: Client, message: Message):
     """ Deletes specific amount of messages you sent. """
-    await self_prune(message)
+    await self_prune(_, message)
