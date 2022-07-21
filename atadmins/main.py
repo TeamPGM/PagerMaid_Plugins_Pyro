1c0b1d33c4f8b51d0f75347a247d63eb55156805
xtaodada <xtao@xtaolink.cn>
2022-07-21 21:40:55 +0800

    atadmins 一键 AT 本群管理员

---
+++ atadmins/main.py
@@ -0,0 +1,22 @@
+from pyrogram.enums import ChatMembersFilter
+
+from pagermaid.listener import listener
+from pagermaid.single_utils import Message
+
+
+@listener(command="atadmins",
+          description="一键 AT 本群管理员（仅在群组中有效）",
+          groups_only=True,
+          parameters="<要说的话>")
+async def at_admins(message: Message):
+    admins = []
+    async for m in message.bot.get_chat_members(message.chat.id, filter=ChatMembersFilter.ADMINISTRATORS):
+        if not m.user.is_bot and not m.user.is_deleted:
+            admins.append(m.user.mention)
+    if not admins:
+        return await message.edit("❌ 没有管理员")
+    say = message.arguments or "召唤本群所有管理员"
+    send_list = ' , '.join(admins)
+    await message.bot.send_message(message.chat.id, "%s：\n\n%s" % (say, send_list),
+                                   reply_to_message_id=message.reply_to_message_id)
+    await message.safe_delete()
