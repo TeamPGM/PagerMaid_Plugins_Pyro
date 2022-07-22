7df86e0ead8c6086e078f6ca6988f4fd265a32e8
xtaodada <xtao@xtaolink.cn>
2022-07-22 15:53:53 +0800

    weather_lite 查询天气

---
+++ weather_lite/main.py
@@ -0,0 +1,23 @@
+from pagermaid.listener import listener
+from pagermaid.single_utils import Message, safe_remove
+
+
+@listener(command="weather_lite",
+          description="查询天气",
+          parameters="<城市>")
+async def weather_lite(message: Message):
+    if not message.arguments:
+        return await message.edit("请输入城市名称")
+    if message.arguments.startswith("_"):
+        return await message.edit("请输入正确的城市名称")
+    city = message.arguments.strip()
+    data = await message.request.get(f"https://zh.wttr.in/{city}.png")
+    if data.status_code != 200:
+        return await message.edit("请输入正确的城市名称")
+    with open("weather.png", "wb") as f:
+        f.write(data.content)
+    await message.reply_photo("weather.png",
+                              reply_to_message_id=message.reply_to_message_id,
+                              quote=False)
+    await message.safe_delete()
+    safe_remove("weather.png")
