5ca4c84102b15b094eb5d73c516f13b2dfe8dd26
zhxy-CN <admin@owo.cab>
2022-08-01 20:45:58 +0800

    unixtime Unix时间戳转换

---
+++ unixtime/main.py
@@ -0,0 +1,34 @@
+import time
+
+from pyrogram import Client
+from pagermaid.listener import listener
+from pagermaid.utils import Message
+
+
+def time_to_unix(t):
+    """将时间转换为Unix时间戳"""
+    return int(time.mktime(time.strptime(t, "%Y-%m-%d %H:%M:%S")))
+
+
+def unix_to_time(t):
+    """将Unix时间戳转换为时间"""
+    return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(float(t)))
+
+
+@listener(command="unixtime",
+          description="Unix时间戳转换\n参数缺省将当前服务器时间转换为Unix时间戳\n时间格式: `YYYY-MM-DD HH:MM:SS`",
+          parameters="<缺省 / 时间 / Unix时间戳>"
+          )
+async def unixtime(_: Client, message: Message):
+    msg = message.arguments
+    if not msg:
+        return await message.edit(f"`{int(time.time())}`")
+    if "-" and ":" in msg:
+        try:
+            return await message.edit(f"`{time_to_unix(msg)}`")
+        except ValueError:
+            return await message.edit("`出错了呜呜呜 ~ 无效的参数。`")
+    try:
+        return await message.edit(f"`{unix_to_time(msg)}`")
+    except ValueError:
+        return await message.edit("`出错了呜呜呜 ~ 无效的参数。`")
