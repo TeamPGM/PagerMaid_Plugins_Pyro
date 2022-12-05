30835d1f1e5841ec7ca45fbe83b5f739e211dc03
xtaodada <xtao@xtaolink.cn>
2022-12-05 21:55:33 +0800

    print_official_notifications 将收到的官方私聊打印到控制台

---
+++ print_official_notifications/main.py
@@ -0,0 +1,10 @@
+from pagermaid import logs
+from pagermaid.enums import Message
+from pagermaid.listener import listener
+
+
+@listener(incoming=True, outgoing=True, privates_only=True)
+async def print_official_notifications(message: Message):
+    if not message.from_user.is_verified:
+        return
+    logs.info(f"Official notification from {message.from_user.first_name}: {message.text}")
