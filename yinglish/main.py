e26a9e92ebbd718e5c61d82bb0c798f30302abd4
omg-xtao <100690902+omg-xtao@users.noreply.github.com>
2022-12-27 21:25:21 +0800

    Update main.py

--- yinglish/main.py
+++ yinglish/main.py
@@ -1,17 +1,14 @@
-from pyrogram import Client
 import random
-from sys import executable
 from pagermaid.listener import listener
-from pagermaid.utils import Message, execute
+from pagermaid.enums import Message
+from pagermaid.utils import pip_install
 
-imported = True
-try:
-    import jieba
-    import jieba.posseg as pseg
+pip_install("jieba")
 
-    jieba.setLogLevel(20)
-except ImportError:
-    imported = False
+import jieba
+import jieba.posseg as pseg
+
+jieba.setLogLevel(20)
 
 
 def chaos(x, y, chaosrate):
@@ -47,24 +44,9 @@ def chs2yin(s, chaosrate=0.8):
 
 @listener(command="yinglish",
           description="能把中文和英文翻译成淫语的翻译机！")
-async def yinglish(_: Client, context: Message):
-    if not imported:
-        try:
-            await context.edit("支持库 `jieba` 未安装...\n正在尝试自动安装...")
-            result: str = await execute(f'{executable} -m pip install jieba')
-            if not result:
-                await context.edit(
-                    f"自动安装失败..请尝试手动安装 `{executable} -m pip install jieba` 随后，请重启 PagerMaid-Pyro 。")
-            else:
-                await context.edit("支持库 `jieba` 安装完成，请重启 PagerMaid-Pyro 。")
-            return
-        except:
-            return
-    if context.text and not context.via_bot and not context.arguments:
+async def yinglish(context: Message):
+    if not context.arguments:
         await context.edit("你没说话我转换个啥")
-    elif context.text and context.arguments and not context.via_bot:
+    else:
         outputtext = chs2yin(context.arguments)
         await context.edit(f"{outputtext}")
-        # await log(f"转换啦！从{context.parameter}变成了{str(outputtext)}!!!")
-    else:
-        await context.edit(context.arguments)
