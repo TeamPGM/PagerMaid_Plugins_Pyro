ffc6947430c434b3de1ccc26c5dc020f4746ffa3
xtaodada <xtao@xtaolink.cn>
2023-01-15 11:16:50 +0800

    dme 适配 v1.2.23

--- dme/main.py
+++ dme/main.py
@@ -1,14 +1,21 @@
 """ Module to message deletion. """
-from pagermaid import bot
-from pagermaid.listener import listener
-from pagermaid.utils import Message, lang
+
+import contextlib
+
+from pagermaid import read_context
+from pagermaid.enums import Client, Message
+from pagermaid.listener import listener, _lock
 from pagermaid.modules.prune import self_prune
+from pagermaid.utils import lang
 
 
 @listener(is_plugin=False, command="dme",
           need_admin=True,
           description=lang('sp_des'),
           parameters=lang('sp_parameters'))
-async def dme(message: Message):
+async def dme(bot: Client, message: Message):
     """ Deletes specific amount of messages you sent. """
+    async with _lock:
+        with contextlib.suppress(Exception):
+            del read_context[(message.chat.id, message.id)]
     await self_prune(bot, message)
