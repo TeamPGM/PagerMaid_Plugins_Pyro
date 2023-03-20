d7648a23c1a75881db261375f83b994b9059d9e0
omg-xtao <100690902+omg-xtao@users.noreply.github.com>
2023-03-20 21:41:21 +0800

    jupai 举牌小人

---
+++ jupai/main.py
@@ -0,0 +1,28 @@
+import urllib.parse
+
+from pagermaid.listener import listener
+from pagermaid.enums import Message
+from pagermaid.utils import lang
+
+ju_pai_api = "https://juapi.org/api/zt.php"
+
+
+@listener(
+    command="jupai",
+    description="生成举牌小人",
+    parameters="[text/reply]"
+)
+async def ju_pai(message: Message):
+    text = message.obtain_message()
+    if not text:
+        return await message.edit(lang('arg_error'))
+    try:
+        image_url = f"{ju_pai_api}?msg={urllib.parse.quote(text)}"
+        await message.reply_photo(
+            image_url,
+            quote=False,
+            reply_to_message_id=message.reply_to_message_id or message.reply_to_top_message_id,
+        )
+        await message.safe_delete()
+    except Exception as e:
+        await message.edit(f"获取失败 ~ {e.__class__.__name__}")
