693b45ea60dbbd7772f5427dd943edb152326129
Ricky8955555 <ricky8955555@gmail.com>
2023-03-29 16:55:29 +0800

    idna morse 支持回复消息

--- idna/DES.md
+++ idna/DES.md
@@ -1,5 +1,5 @@
 国际化域名 (IDNA) 编码转换工具
 
 指令:
-- `,punyencode <待编码内容>` (编码至 Punycode)
-- `,punydecode <待解码内容>` (从 Punycode 解码)
+- `,punyencode <待编码内容> (支持回复消息)` (编码至 Punycode)
+- `,punydecode <待解码内容> (支持回复消息)` (从 Punycode 解码)
--- idna/main.py
+++ idna/main.py
@@ -5,25 +5,23 @@ from pagermaid.enums import Message
 from pagermaid.listener import listener
 
 
-@listener(command="punyencode", description="编码至 Punycode", parameters="[待编码内容]")
-async def punyencode(message: Message) -> None:
-    if message.arguments:
-        try:
-            result = message.arguments.encode("idna").decode()
-        except Exception:
-            result = "呜呜呜 ~ 转换失败了，可能含有非法字符。"
-    else:
-        result = "请输入参数"
-    await message.edit(f"`{result}`")
+@listener(command="punyencode", description="编码至 Punycode", parameters="[待编码内容] (支持回复消息)")
+async def punyencode(message: Message):
+    if not (text := message.obtain_message()):
+        return await message.edit("请输入参数")
+    try:
+        encoded = text.encode("idna").decode()
+    except Exception:
+        return await message.edit("呜呜呜 ~ 转换失败了，可能含有非法字符。")
+    await message.edit(f"`{encoded}`")
 
 
-@listener(command="punydecode", description="从 Punycode 解码", parameters="[待解码内容]")
-async def punydecode(message: Message) -> None:
-    if message.arguments:
-        try:
-            result = message.arguments.encode().decode("idna")
-        except Exception:
-            result = "呜呜呜 ~ 转换失败了，可能含有非法字符。"
-    else:
-        result = "请输入参数"
-    await message.edit(f"`{result}`")
+@listener(command="punydecode", description="从 Punycode 解码", parameters="[待解码内容] (支持回复消息)")
+async def punydecode(message: Message):
+    if not (text := message.obtain_message()):
+        return await message.edit("请输入参数")
+    try:
+        decoded = text.encode().decode("idna")
+    except Exception:
+        return await message.edit("呜呜呜 ~ 转换失败了，可能含有非法字符。")
+    await message.edit(f"`{decoded}`")
