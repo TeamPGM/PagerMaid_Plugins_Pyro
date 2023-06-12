01580b0b3b69e12fd7c9a3f7dfad6699559423f5
Sourcery AI <>
2023-06-12 05:26:29 +0000

    'Refactored by Sourcery'

--- shift/main.py
+++ shift/main.py
@@ -1,4 +1,5 @@
 """ PagerMaid module for channel help. """
+
 from asyncio import sleep
 from functools import partial
 from random import uniform
@@ -18,7 +19,7 @@ import contextlib
 
 
 WHITELIST = [-1001441461877]
-AVAILABLE_OPTIONS = set(("nosender", "nocaption", "silent"))
+AVAILABLE_OPTIONS = {"nosender", "nocaption", "silent"}
 
 
 def try_cast_or_fallback(val: Any, t: type) -> Any:
@@ -182,97 +183,96 @@ def forward(
         disable_notification: bool = False,
         remove_caption: bool = False
     ):
-    if as_copy:
-        if message.service:
-            raise ValueError("Unable to copy service messages")
-
-        if message.game:
-            raise ValueError("Users cannot send messages with Game media type")
-
-        # TODO: Improve markdown parser. Currently html appears to be more stable, thus we use it here because users
-        #       can"t choose.
-
-        if message.text:
-            return message._client.send_message(
-                chat_id,
-                text=message.text.html,
-                parse_mode=ParseMode.HTML,
-                disable_web_page_preview=not message.web_page,
-                disable_notification=disable_notification
-            )
-        elif message.media:
-            caption = message.caption.html if message.caption and not remove_caption else None
-
-            send_media = partial(
-                message._client.send_cached_media,
-                chat_id=chat_id,
-                disable_notification=disable_notification
-            )
-
-            if message.photo:
-                file_id = message.photo.file_id
-            elif message.audio:
-                file_id = message.audio.file_id
-            elif message.document:
-                file_id = message.document.file_id
-            elif message.video:
-                file_id = message.video.file_id
-            elif message.animation:
-                file_id = message.animation.file_id
-            elif message.voice:
-                file_id = message.voice.file_id
-            elif message.sticker:
-                file_id = message.sticker.file_id
-            elif message.video_note:
-                file_id = message.video_note.file_id
-            elif message.contact:
-                return message._client.send_contact(
-                    chat_id,
-                    phone_number=message.contact.phone_number,
-                    first_name=message.contact.first_name,
-                    last_name=message.contact.last_name,
-                    vcard=message.contact.vcard,
-                    disable_notification=disable_notification
-                )
-            elif message.location:
-                return message._client.send_location(
-                    chat_id,
-                    latitude=message.location.latitude,
-                    longitude=message.location.longitude,
-                    disable_notification=disable_notification
-                )
-            elif message.venue:
-                return message._client.send_venue(
-                    chat_id,
-                    latitude=message.venue.location.latitude,
-                    longitude=message.venue.location.longitude,
-                    title=message.venue.title,
-                    address=message.venue.address,
-                    foursquare_id=message.venue.foursquare_id,
-                    foursquare_type=message.venue.foursquare_type,
-                    disable_notification=disable_notification
-                )
-            elif message.poll:
-                return message._client.send_poll(
-                    chat_id,
-                    question=message.poll.question,
-                    options=[opt.text for opt in message.poll.options],
-                    disable_notification=disable_notification
-                )
-            elif message.game:
-                return message._client.send_game(
-                    chat_id,
-                    game_short_name=message.game.short_name,
-                    disable_notification=disable_notification
-                )
-            else:
-                raise ValueError("Unknown media type")
-
-            if message.sticker or message.video_note:  # Sticker and VideoNote should have no caption
-                return send_media(file_id)
-            else:
-                return send_media(file_id=file_id, caption=caption, parse_mode=ParseMode.HTML)  # type: ignore
-        else:
-            raise ValueError("Can't copy this message")
-    else:
+    if not as_copy:
         return message.forward(chat_id, disable_notification)
+    if message.service:
+        raise ValueError("Unable to copy service messages")
+
+    if message.game:
+        raise ValueError("Users cannot send messages with Game media type")
+
+    # TODO: Improve markdown parser. Currently html appears to be more stable, thus we use it here because users
+    #       can"t choose.
+
+    if message.text:
+        return message._client.send_message(
+            chat_id,
+            text=message.text.html,
+            parse_mode=ParseMode.HTML,
+            disable_web_page_preview=not message.web_page,
+            disable_notification=disable_notification
+        )
+    elif message.media:
+        caption = message.caption.html if message.caption and not remove_caption else None
+
+        send_media = partial(
+            message._client.send_cached_media,
+            chat_id=chat_id,
+            disable_notification=disable_notification
+        )
+
+        if message.photo:
+            file_id = message.photo.file_id
+        elif message.audio:
+            file_id = message.audio.file_id
+        elif message.document:
+            file_id = message.document.file_id
+        elif message.video:
+            file_id = message.video.file_id
+        elif message.animation:
+            file_id = message.animation.file_id
+        elif message.voice:
+            file_id = message.voice.file_id
+        elif message.sticker:
+            file_id = message.sticker.file_id
+        elif message.video_note:
+            file_id = message.video_note.file_id
+        elif message.contact:
+            return message._client.send_contact(
+                chat_id,
+                phone_number=message.contact.phone_number,
+                first_name=message.contact.first_name,
+                last_name=message.contact.last_name,
+                vcard=message.contact.vcard,
+                disable_notification=disable_notification
+            )
+        elif message.location:
+            return message._client.send_location(
+                chat_id,
+                latitude=message.location.latitude,
+                longitude=message.location.longitude,
+                disable_notification=disable_notification
+            )
+        elif message.venue:
+            return message._client.send_venue(
+                chat_id,
+                latitude=message.venue.location.latitude,
+                longitude=message.venue.location.longitude,
+                title=message.venue.title,
+                address=message.venue.address,
+                foursquare_id=message.venue.foursquare_id,
+                foursquare_type=message.venue.foursquare_type,
+                disable_notification=disable_notification
+            )
+        elif message.poll:
+            return message._client.send_poll(
+                chat_id,
+                question=message.poll.question,
+                options=[opt.text for opt in message.poll.options],
+                disable_notification=disable_notification
+            )
+        elif message.game:
+            return message._client.send_game(
+                chat_id,
+                game_short_name=message.game.short_name,
+                disable_notification=disable_notification
+            )
+        else:
+            raise ValueError("Unknown media type")
+
+        if message.sticker or message.video_note:  # Sticker and VideoNote should have no caption
+            return send_media(file_id)
+        else:
+            return send_media(file_id=file_id, caption=caption, parse_mode=ParseMode.HTML)  # type: ignore
+    else:
+        raise ValueError("Can't copy this message")
