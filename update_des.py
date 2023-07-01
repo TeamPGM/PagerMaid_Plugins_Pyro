c317d4ba6f1f35204e86a8a3f2258881721b5f6e
xtaodada <xtao@xtaolink.cn>
2023-07-01 20:07:18 +0800

    CI: Alpha source

--- update_des.py
+++ update_des.py
@@ -3,14 +3,16 @@ import os
 
 
 def update_des():
-    with open("list.json", "r", encoding="utf8") as f:
-        list_json = json.load(f)
-    for plugin in list_json["list"]:
-        if os.path.exists(f"{plugin['name']}{os.sep}DES.md"):
-            with open(f"{plugin['name']}{os.sep}DES.md", "r", encoding="utf8") as f:
-                plugin["des"] = f.read().strip()
-    with open("list.json", "w", encoding="utf8") as f:
-        json.dump(list_json, f, ensure_ascii=False, indent=4)
+    list_json_start = ["", "alpha/"]
+    for start in list_json_start:
+        with open(f"{start}list.json", "r", encoding="utf8") as f:
+            list_json = json.load(f)
+        for plugin in list_json["list"]:
+            if os.path.exists(f"{start}{plugin['name']}{os.sep}DES.md"):
+                with open(f"{start}{plugin['name']}{os.sep}DES.md", "r", encoding="utf8") as f:
+                    plugin["des"] = f.read().strip()
+        with open(f"{start}list.json", "w", encoding="utf8") as f:
+            json.dump(list_json, f, ensure_ascii=False, indent=4)
 
 
 if __name__ == "__main__":
--- update_list.py
+++ update_list.py
@@ -7,43 +7,50 @@ from update_des import update_des
 
 main = get("https://api.github.com/repos/TeamPGM/PagerMaid_Plugins_Pyro/commits/v2").json()
 plugins = []
+alpha_plugins = []
+list_json_start = ["", "alpha/"]
 for file in main["files"]:
-    if file["filename"] == "list.json":
+    if "list.json" in file["filename"]:
         print(main['sha'] + " no need！")
         exit()
     if "/main.py" in file["filename"]:
-        plugins.append(file["filename"].split("/")[0])
+        if file["filename"].startswith("alpha"):
+            alpha_plugins.append(file["filename"].split("/")[1])
+        else:
+            plugins.append(file["filename"].split("/")[0])
 delete = bool(main['commit']['message'].startswith("Delete"))
 
-with open("list.json", "r", encoding="utf8") as f:
-    list_json = json.load(f)
-for plugin in plugins:
-    exist = False
-    for plug_dict in list_json["list"]:
-        if plug_dict["name"] == plugin:
-            exist = True
-            old_version = decimal.Decimal(plug_dict["version"])
-            plug_dict["version"] = old_version + decimal.Decimal("0.01")
-            plug_dict["size"] = f"{os.path.getsize(f'{plugin}{os.sep}main.py') / 1000} kb"
-            if delete:
-                list_json["list"].remove(plug_dict)
-            break
-    if not exist:
-        short_des = main['commit']['message'].split("\nCo-authored-by")[0].strip()
-        list_json["list"].append(
-            {
-                "name": plugin,
-                 "version": "1.0",
-                 "section": "chat",
-                 "maintainer": main['commit']['author']['name'],
-                 "size": f"{os.path.getsize(f'{plugin}{os.sep}main.py') / 1000} kb",
-                 "supported": True,
-                 "des_short": short_des,
-                 "des": "",
-            }
-        )
-with open("list.json", "w", encoding="utf8") as f:
-    json.dump(list_json, f, ensure_ascii=False, indent=4)
+
+for idx, plugins_ in enumerate([plugins, alpha_plugins]):
+    with open(f"{list_json_start[idx]}list.json", "r", encoding="utf8") as f:
+        list_json = json.load(f)
+    for plugin in plugins_:
+        exist = False
+        for plug_dict in list_json["list"]:
+            if plug_dict["name"] == plugin:
+                exist = True
+                old_version = decimal.Decimal(plug_dict["version"])
+                plug_dict["version"] = old_version + decimal.Decimal("0.01")
+                plug_dict["size"] = f"{os.path.getsize(f'{list_json_start[idx]}{plugin}{os.sep}main.py') / 1000} kb"
+                if delete:
+                    list_json["list"].remove(plug_dict)
+                break
+        if not exist:
+            short_des = main['commit']['message'].split("\nCo-authored-by")[0].strip()
+            list_json["list"].append(
+                {
+                    "name": plugin,
+                    "version": "1.0",
+                    "section": "chat",
+                    "maintainer": main['commit']['author']['name'],
+                    "size": f"{os.path.getsize(f'{list_json_start[idx]}{plugin}{os.sep}main.py') / 1000} kb",
+                    "supported": True,
+                    "des_short": short_des,
+                    "des": "",
+                }
+            )
+    with open(f"{list_json_start[idx]}list.json", "w", encoding="utf8") as f:
+        json.dump(list_json, f, ensure_ascii=False, indent=4)
 
 update_des()
 
