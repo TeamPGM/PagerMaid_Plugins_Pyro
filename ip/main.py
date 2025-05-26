47a95bdf0bf433880e38c2fbe7ea07efe7d4d07e
Godlike <26649232+godlikeanyone@users.noreply.github.com>
2025-05-26 21:00:20 +0800

    ip Lookup Priority: User-Specified Argument First (#194)

--- ip/main.py
+++ ip/main.py
@@ -65,7 +65,14 @@ async def ipinfo(message: Message):
     reply = message.reply_to_message
     message: Message = await message.edit("正在查询中...")
     try:
-        if reply:
+        if message.arguments:
+            url_ = urlparse(message.arguments)
+            url = url_.hostname or url_.path
+            if ":" in url and "." not in url and not url_.hostname:
+                url = message.arguments
+            await message.edit(await get_ip_info(url))
+            return
+        elif reply:
             for num in range(len(reply.entities)):
                 url = reply.text[
                     reply.entities[num].offset : reply.entities[num].offset
@@ -75,13 +82,6 @@ async def ipinfo(message: Message):
                 url = url.hostname or url.path
                 await message.edit(await get_ip_info(url))
                 return
-        elif message.arguments:
-            url_ = urlparse(message.arguments)
-            url = url_.hostname or url_.path
-            if ":" in url and "." not in url and not url_.hostname:
-                url = message.arguments
-            await message.edit(await get_ip_info(url))
-            return
         await message.edit("没有找到要查询的 ip/域名 ...")
     except Exception:
         await message.edit("没有找到要查询的 ip/域名 ...")
